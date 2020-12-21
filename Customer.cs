6c738b707f8c082d6f1898c97ffdb2ddd409ee25
Megan Fontenot <meganfontenot517@gmail.com>
2020-12-21 11:27:08 -0600

    parent/child classes created and inheritance assigned.

---
+++ Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soda_Machine
+{
+    class Customer
+    {
+        // member variables
+
+        // constructor
+
+        // member methods
+    }
+}
